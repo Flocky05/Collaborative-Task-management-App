0b349573473cceb6b81808a69b3025d0d30f294f
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-08 02:39:22 +0600

    added my task section

--- src/component/My-Task/MyTask.jsx
+++ src/component/My-Task/MyTask.jsx
@@ -1,9 +1,49 @@
-
+import task1 from "../../assets/images/my task/task1.avif"
+import task2 from "../../assets/images/my task/task2.avif"
+import task4 from "../../assets/images/my task/task4.avif"
 
 const MyTask = () => {
     return (
         <div>
-            <p>This is Task section , where you can add some task</p>
+            <h2 className="text-3xl font-bold text-center p-4">The project that have to done...</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 p-16">
+                <div className="border-2 rounded-lg">
+                    <img className="w-full h-60" src={task1} alt="" />
+                    <div className="p-3">
+                    <div>
+                        <h2 className="text-2xl font-semibold">Business Start</h2>
+                        <p>3-07-23 to <span className="font-bold text-red-600">21</span> -10-23</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ad.</p>
+                    <p>due: <span className="font-bold text-red-500">13%</span></p>
+                    <a className="bg-blue-600 text-white px-3 py-1 rounded-lg" href="">More</a>
+                    </div>
+                </div>
+                <div className="border-2 rounded-lg">
+                    <img className="w-full h-60" src={task2} alt="" />
+                    <div className="p-3">
+                    <div>
+                        <h2 className="text-2xl font-semibold">Dolor Sit AMET</h2>
+                        <p>24-02-23 to <span className="font-bold text-red-600">01</span> -10-23</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ad.</p>
+                    <p>due: <span className="font-bold text-red-500">13%</span></p>
+                    <a className="bg-blue-600 text-white px-3 py-1 rounded-lg" href="">More</a>
+                    </div>
+                </div>
+                <div className="border-2 rounded-lg">
+                    <img className="w-full h-60" src={task4} alt="" />
+                    <div className="p-3">
+                    <div>
+                        <h2 className="text-2xl font-semibold">Bluid your Plans</h2>
+                        <p>78-03-23 to <span className="font-bold text-red-600">14</span> -10-23</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, ad.</p>
+                    <p>due: <span className="font-bold text-red-500">13%</span></p>
+                    <a className="bg-blue-600 text-white px-3 py-1 rounded-lg" href="">More</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/SignUp/SignUp.jsx
+++ src/component/SignUp/SignUp.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const SignUp = () => {
@@ -14,10 +15,13 @@ const SignUp = () => {
                     <textarea name="text" id="bio" rows="8" className="w-3/4 bg-white border-2 p-2 rounded-lg" placeholder="add your bio"></textarea>
                     <h3 className="text-lg font-semibold">Password</h3>
                     <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="password" name="password" id="password" placeholder="Enter your password" />
+                    <h3 className="text-lg font-semibold mt-3">Upload your Image</h3>
                     <input type="file" id="img" name="img" accept="image/*"/>
-                    <input className="bg-blue-500 rounded-full text-white w-40 px-4 py-2 shadow-xl mt-2" type="submit" />
-                
-                
+                    <input className="bg-blue-500 rounded-full text-white w-40 px-4 py-2 shadow-xl mt-3" type="submit" />
+                    <div className="flex items-center mt-3">
+                    <p className="text-gray-600">I have Account?</p>
+                    <Link className="text-lg font-semibold" to="/login">Login</Link>
+                </div>
             </div>
         </div>
     );
