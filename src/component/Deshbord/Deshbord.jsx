e4aacf774dc4e9e32c124e403326cca90b6081a6
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-08 20:58:11 +0600

    fixed react router issue

--- src/component/Deshbord/Deshbord.jsx
+++ src/component/Deshbord/Deshbord.jsx
@@ -1,5 +1,4 @@
 
-
 const Deshbord = () => {
     return (
         <div>
--- src/component/Inbox/Inbox.jsx
+++ src/component/Inbox/Inbox.jsx
@@ -4,33 +4,36 @@ import task4 from "../../assets/images/my task/task4.avif"
 import task5 from "../../assets/images/my task/task5.jpg"
 import task6 from "../../assets/images/my task/task6.jpg"
 import task7 from "../../assets/images/my task/task7.jpg"
+import Dashboard from "../Deshbord/Deshbord"
 
 const Inbox = () => {
     return (
-        <div className="flex">
+        <div>
+
+<div className="flex">
             <div className="bg-orange-200 w-1/4 min-h-screen">
                 <div className="flex p-2 items-center ">
-                    <img className="w-16 h-16 rounded-full" src={task1} alt="" />
+                    <img className="w-10 h-10 rounded-full" src={task1} alt="" />
                     <h3 className="text-lg px-2">Business Start</h3>
                 </div>
                 <div className="flex p-2 items-center bg-blue-100">
-                    <img className="w-16 h-16 rounded-full" src={task2} alt="" />
+                    <img className="w-10 h-10 rounded-full" src={task2} alt="" />
                     <h3 className="text-lg px-2">Dolor Sit AMET</h3>
                 </div>
                 <div className="flex p-2 items-center">
-                    <img className="w-16 h-16 rounded-full" src={task4} alt="" />
+                    <img className="w-10 h-10 rounded-full" src={task4} alt="" />
                     <h3 className="text-lg px-2">Bluid your Plans</h3>
                 </div>
                 <div className="flex p-2 items-center ">
-                    <img className="w-16 h-16 rounded-full" src={task5} alt="" />
+                    <img className="w-10 h-10 rounded-full" src={task5} alt="" />
                     <h3 className="text-lg px-2">Compling</h3>
                 </div>
                 <div className="flex p-2 items-center ">
-                    <img className="w-16 h-16 rounded-full" src={task6} alt="" />
+                    <img className="w-10 h-10 rounded-full" src={task6} alt="" />
                     <h3 className="text-lg px-2">Digital Marketing</h3>
                 </div>
                 <div className="flex m-2 p-1 items-center border-2">
-                    <img className="w-16 h-16 rounded-full" src={task7} alt="" />
+                    <img className="w-10 h-10 rounded-full" src={task7} alt="" />
                     <h3 className="text-lg px-2">Upholes</h3>
                 </div>
             </div>
@@ -40,6 +43,8 @@ const Inbox = () => {
                     <h3 className="text-lg px-2">Digital Marketing</h3>
                 </div>
             </div>
+        </div>
+        <Dashboard></Dashboard>
         </div>
     );
 };
