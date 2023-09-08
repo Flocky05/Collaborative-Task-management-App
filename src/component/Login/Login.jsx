0b2644dabac1a1ba6e8691e33335feed8acbfdfb
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-08 17:40:56 +0600

    all set up of authentication

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,18 +1,54 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase/firebase.config";
 
+const auth = getAuth(app);
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import img from "../../assets/images/login.avif"
 const Login = () => {
+    const[error,setError]=useState();
+    const[success,setSuccess]=useState();
+    const handleLogin=(event)=>{
+        event.preventDefault();
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(email,password);
+        setError(" ");
+        setSuccess(" ");
+        if(password.length<6){
+            setError("Password must be at least 6 character");
+            return
+        }
+        signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    setSuccess("User successully logedin");
+    setError(" ");
+    form.reset();
+    // ...
+  })
+  .catch((error) => {
+    setError(error.message);
+    form.reset();
+  });
+    }
     return (
         <div className="grid grid-cols-2 items-center">
-            <div className="flex flex-col p-10">
+            <div className="p-10">
                 <h3 className="text-3xl font-bold">Register Now</h3>
-                <input className="bg-white border-2 my-2 rounded-lg p-2" type="email" name="email" id="email" placeholder="Enter your email"  />
-                <input className="bg-white border-2 my-2 rounded-lg p-2" type="password" name="password" id="password" placeholder="Enter your password"  />
-                <input className="bg-blue-500 rounded-full text-white w-40 px-4 py-2 shadow-xl" type="submit" />
-                <div className="flex items-center">
-                    <p className="text-gray-600">Don't have Account?</p>
-                    <Link className="text-lg font-semibold" to="/signUp">SignUp</Link>
-                </div>
+                <form className="flex flex-col" onSubmit={handleLogin}>
+                    <input className="bg-white border-2 my-2 rounded-lg p-2" type="email" name="email" id="email" placeholder="Enter your email" required />
+                    <input className="bg-white border-2 my-2 rounded-lg p-2" type="password" name="password" id="password" placeholder="Enter your password" required />
+                    <input className="bg-blue-500 rounded-full text-white w-40 px-4 py-2 shadow-xl" type="submit" />
+                    <div className="flex items-center">
+                        <p className="text-gray-600">Don't have Account?</p>
+                        <Link className="text-lg font-semibold" to="/signUp">SignUp</Link>
+                    </div>
+                </form>
+                <p className="text-red-500 text-xl font-semibold text-center">{error}</p>
+                <p className="text-green-500 text-xl font-semibold text-center">{success}</p>
             </div>
             <img src={img} alt="" />
         </div>
--- src/component/SignUp/SignUp.jsx
+++ src/component/SignUp/SignUp.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import { getAuth ,createUserWithEmailAndPassword } from "firebase/auth";
 import app from "../../firebase/firebase.config";
+import { useState } from "react";
 
 const auth = getAuth(app);
 const SignUp = () => {
+    const[error,setError]=useState();
+    const[success,setSuccess]=useState();
     const handleSubmit =(event)=>{
-        event.preventDefault()
+        event.preventDefault();
+        setSuccess(" ");
         const email=event.target.email.value;
         const name=event.target.name.value;
         const bio=event.target.bio.value;
@@ -17,10 +21,15 @@ const SignUp = () => {
     // Signed in 
     const SignedUser = result.user;
     console.log(SignedUser);
+    setError(" ");
+    event.target.reset();
+    setSuccess("Sucessfully SignUp");
     // ...
   })
   .catch((error) => {
-   console.error(error);
+   console.error(error.message);
+   setError(error.message);
+   setSuccess(" ");
     // ..
   });
     }
@@ -30,13 +39,13 @@ const SignUp = () => {
                 <h3 className="text-3xl font-bold">SignUp Now</h3>
                 <form onSubmit={handleSubmit}>
                 <h3 className="text-lg font-semibold mt-8">Your Name</h3>
-                    <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="text" name="name" id="name" placeholder="Enter your Name"  />
+                    <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="text" name="name" id="name" placeholder="Enter your Name" required />
                     <h3 className="text-lg font-semibold">Your Email</h3>
-                    <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="email" name="email" id="email" placeholder="Enter your email"  />
+                    <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="email" name="email" id="email" placeholder="Enter your email" required />
                     <h3 className="text-lg font-semibold">About Bio </h3>
-                    <textarea name="text" id="bio" rows="8" className="w-3/4 bg-white border-2 p-2 rounded-lg" placeholder="add your bio"></textarea>
+                    <textarea name="text" id="bio" rows="8" className="w-3/4 bg-white border-2 p-2 rounded-lg" placeholder="add your bio" required></textarea>
                     <h3 className="text-lg font-semibold">Password</h3>
-                    <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="password" name="password" id="password" placeholder="Enter your password" />
+                    <input className="bg-white border-2 my-2 rounded-lg p-2 w-3/4" type="password" name="password" id="password" placeholder="Enter your password" required/>
                     <h3 className="text-lg font-semibold mt-3">Upload your Image</h3>
                     <input type="file" id="img" name="img" accept="image/*"/>
                     <input className="bg-blue-500 rounded-full text-white w-40 px-4 py-2 shadow-xl mt-3" type="submit" />
@@ -46,6 +55,8 @@ const SignUp = () => {
                     </div>
                 </form>
             </div>
+            <p className="text-red-500 text-xl font-semibold text-center">{error}</p>
+            <p className="text-green-500 text-xl font-semibold text-center">{success}</p>
         </div>
     );
 };
